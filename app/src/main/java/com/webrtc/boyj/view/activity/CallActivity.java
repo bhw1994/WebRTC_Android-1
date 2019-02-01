1e714024743e7ba5abef36586f6161aedb45212d
bhw1994 <bhw1994@gmail.com>
2019-02-01 17:00:57 +0900

    add webRTC feature.     add webrtc dependency     add retrofit , gson , okhttp dependency for get ice server list.     add peerconnetion client.     add ice server
        add camera , audio permission for webrtc
        add App class for global context

--- app/src/main/java/com/webrtc/boyj/view/activity/CallActivity.java
+++ app/src/main/java/com/webrtc/boyj/view/activity/CallActivity.java
@@ -1,29 +1,57 @@
 package com.webrtc.boyj.view.activity;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.webrtc.boyj.BR;
 import com.webrtc.boyj.R;
+import com.webrtc.boyj.api.peer.PeerConnectionClient;
+import com.webrtc.boyj.api.peer.manager.EglBaseManager;
+import com.webrtc.boyj.api.peer.manager.RendererManager;
 import com.webrtc.boyj.databinding.ActivityCallBinding;
 import com.webrtc.boyj.model.dto.User;
 import com.webrtc.boyj.utils.Constants;
+import com.webrtc.boyj.utils.Logger;
 import com.webrtc.boyj.viewmodel.CallViewModel;
+
+import org.webrtc.VideoTrack;
 
 public class CallActivity extends BaseActivity<ActivityCallBinding, CallViewModel> {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final User user = (User) getIntent().getSerializableExtra(Constants.EXTRA_USER);
+        Intent intent=getIntent();
 
-        if(user != null) {
-            binding.setVariable(BR.item, user);
+        boolean iscaller=intent.getBooleanExtra("caller",true);
+
+
+        RendererManager rendererManager=new RendererManager(EglBaseManager.getEglBase());
+        rendererManager.initSurfaeView(binding.localView);
+        rendererManager.initSurfaeView(binding.remoteView);
+
+        model.getIsActive().observe(this, isActive -> finish());
+        if(iscaller){
+            String deviceToken=intent.getStringExtra("deviceToken");
+            model.call(deviceToken);
         }
 
-        // Activity 종료 플래그
-        model.getIsActive().observe(this, isActive -> finish());
+        else{
+            String room=intent.getStringExtra("room");
+            model.accept(room);
+        }
+
+
+        PeerConnectionClient.getInstance().remoteMediaStreamSubject.subscribe(mediaStream -> {
+            Logger.d("remotemediastream received");
+            VideoTrack videoTrack = mediaStream.videoTracks.get(0);
+            videoTrack.addSink(binding.remoteView);
+            PeerConnectionClient.getInstance().localStreamTo(binding.localView);
+        });
+
 
     }
 
